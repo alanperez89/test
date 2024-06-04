cba09b832904f2b3201cd4fe001280417ea1dc89
Jesús Alán Pérez Aguirre <alanix89@gmail.com>
2024-06-04 04:49:42 +0000

    se modificaron pruebas unitarias a dimensiones

--- models/example/staging/dim_customer_profile.sql
+++ models/example/staging/dim_customer_profile.sql
@@ -2,7 +2,7 @@ WITH data_raw AS (
     SELECT * FROM {{ref('raw_bank_marketing')}}   -- se importa la raw data
 ),
 
-data_transform AS (      -- se construye la tabla dim_customer_profile y se filtra el resultado de la encuesta (conversion)
+data_transform AS (      -- se construye la tabla dim_customer_profile y se filtra el resultado de la encuesta (conversion) y los unknown
     SELECT
         DISTINCT job,
         marital,
